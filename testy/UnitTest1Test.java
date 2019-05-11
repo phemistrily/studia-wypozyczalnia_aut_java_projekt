a76da3f511d99acf32d6f858424cfb625562a6fe
krystian19971997 <krystian19971997@interia.pl>
2019-05-11 20:24:43 +0200

    wrzucam testy

--- testy/UnitTest1Test.java
+++ testy/UnitTest1Test.java
@@ -4,12 +4,9 @@ import static org.junit.Assert.*;
 
 public class UnitTest1Test
 {
-    @Test
-    public void test()
-    {
-        UnitTest1 test = new UnitTest1();
-        int result = test.addNumber(5,2);
-        assertEquals(7,result);
 
+    @Test
+    public void addNumber()
+    {
     }
 }
