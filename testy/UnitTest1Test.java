19a373e59b429a0071a30e3839305c46324becbd
krystian19971997 <krystian19971997@interia.pl>
2019-05-11 21:07:15 +0200

    testuje i dzialaja testy jednostkowe

--- testy/UnitTest1Test.java
+++ testy/UnitTest1Test.java
@@ -6,7 +6,10 @@ public class UnitTest1Test
 {
 
     @Test
-    public void addNumber()
+    public void test()
     {
+        UnitTest1 test = new UnitTest1();
+        int result = test.addNumber(5,2);
+        assertEquals(7,result);
     }
 }
