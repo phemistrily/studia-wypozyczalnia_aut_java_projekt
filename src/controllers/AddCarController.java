7ec06a3174bc993d20ba0e74906f4036977bede0
krystian19971997 <krystian19971997@interia.pl>
2019-05-23 00:12:48 +0200

    dodaje nowy kontroler AddCarController oraz widok do niego

---
+++ src/controllers/AddCarController.java
@@ -0,0 +1,62 @@
+package controllers;
+
+import entities.UserEntity;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AddCarController  {
+
+    @FXML
+    private AnchorPane addCarPanel;
+
+    @FXML
+    private TextField idLabel;
+
+    @FXML
+    private TextField nameLabel;
+
+    @FXML
+    private TextField classLabel;
+
+    @FXML
+    private TextField brandLabel;
+
+    @FXML
+    private TextField localisationLabel;
+
+    @FXML
+    void addCarToDb(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backToMain(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getCatalogView(ActionEvent event) {
+
+    }
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
