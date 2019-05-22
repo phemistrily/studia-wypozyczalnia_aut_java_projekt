4ba0b74472b6958bf3a952f898ceb60b7d4381af
krystian19971997 <krystian19971997@interia.pl>
2019-05-23 01:18:22 +0200

    Filtrowanie by Krystian :D !

--- src/controllers/carViewController.java
+++ src/controllers/carViewController.java
@@ -3,15 +3,16 @@ package controllers;
 import entities.CarsEntity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyEvent;
 import models.CarsTableModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 
 import entities.UserEntity;
@@ -21,20 +22,26 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import javax.xml.soap.Text;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 public class carViewController implements Initializable
 {
 
+    @FXML
+    private TextField localisationBTN;
+    @FXML
+    private TextField ByClassBTN;
+    @FXML
     public Button bookCarBtn;
     @FXML
     private AnchorPane carView;
@@ -56,19 +63,26 @@ public class carViewController implements Initializable
     private ObservableList<CarsTableModel> carsTableModel = FXCollections.observableArrayList();
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL location, ResourceBundle resources)
+    {
         this.initializeFactory();
-        try {
+        try
+        {
             this.getCars();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             e.printStackTrace();
         }
         carCatalogTableView.setItems(carsTableModel);
     }
 
-    private void initializeFactory() {
+    FilteredList filter = new FilteredList(carsTableModel, e -> true);
+
+    private void initializeFactory()
+    {
         lp.setCellValueFactory(new PropertyValueFactory<>("lp"));
         name.setCellValueFactory(new PropertyValueFactory<>("name"));
         carClass.setCellValueFactory(new PropertyValueFactory<>("carClass"));
@@ -78,14 +92,18 @@ public class carViewController implements Initializable
     }
 
 
-    private void getCars() throws IOException, SQLException {
+
+    private void getCars() throws IOException, SQLException
+    {
         CarsEntity cars = new CarsEntity();
         ResultSet carsData = cars.getCars();
         this.putCarsToCarsTableModel(carsData);
     }
 
-    private void putCarsToCarsTableModel(ResultSet carsData) throws SQLException {
-        while (carsData.next()){
+    private void putCarsToCarsTableModel(ResultSet carsData) throws SQLException
+    {
+        while (carsData.next())
+        {
             carsTableModel.add(new CarsTableModel(carsData.getInt("lp"), carsData.getString("name"), carsData.getString("carClass"),
                     carsData.getString("brand"), carsData.getString("is_rented"), carsData.getString("localisation")));
         }
@@ -125,5 +143,44 @@ public class carViewController implements Initializable
     public void backAction(ActionEvent event)
     {
 
+    }
+
+    public void filtrByLocalisation(javafx.scene.input.KeyEvent keyEvent)
+    {
+        localisationBTN.textProperty().addListener((observable, oldValue, newValue) -> {
+            filter.setPredicate((Predicate<? super CarsTableModel>)(CarsTableModel ctb)->{
+                if(newValue.isEmpty() || newValue == null)
+                {
+                    return true;
+                }
+                else if(ctb.getLocalisation().contains(newValue)){
+                    return true;
+                }
+                return false;
+            });
+        });
+        SortedList sort = new SortedList(filter);
+        sort.comparatorProperty().bind(carCatalogTableView.comparatorProperty());
+        carCatalogTableView.setItems(sort);
+    }
+
+
+    public void filtrByClass(KeyEvent keyEvent)
+    {
+        ByClassBTN.textProperty().addListener((observable, oldValue, newValue) -> {
+            filter.setPredicate((Predicate<? super CarsTableModel>)(CarsTableModel ctb)->{
+                if(newValue.isEmpty() || newValue == null)
+                {
+                    return true;
+                }
+                else if(ctb.getCarClass().contains(newValue)){
+                    return true;
+                }
+                return false;
+            });
+        });
+        SortedList sort = new SortedList(filter);
+        sort.comparatorProperty().bind(carCatalogTableView.comparatorProperty());
+        carCatalogTableView.setItems(sort);
     }
 }
